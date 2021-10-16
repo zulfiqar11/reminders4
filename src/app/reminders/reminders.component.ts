bb3a2dd1f8fe19b61b23f09bb79ef64f77fb174d
zulfiqarsyed <zulfiqarsyed@yahoo.com>
2021-10-15 17:15:21 -0700

    refactored hardcoded values from front to backend

--- src/app/reminders/reminders.component.ts
+++ src/app/reminders/reminders.component.ts
@@ -39,6 +39,82 @@ export class RemindersComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'phoneNumber', 'emailAddress', 'frequency', 'month',  'day', 'week',  'weekday',  'time', 'date','message'];
 
+  frequencies = [
+    {value: 'Once', viewValue: 'Once'},
+    {value: 'Daily', viewValue: 'Daily'},
+    {value: 'Weekly', viewValue: 'Weekly'},
+    {value: 'Monthly', viewValue: 'Monthly'},
+    {value: 'MonthWeekly', viewValue: 'MonthWeekly'},
+    {value: 'Yearly', viewValue: 'Yearly'}
+  ];
+
+  weeks = [
+    {value: 'First', viewValue: 'First'},
+    {value: 'Second', viewValue: 'Second'},
+    {value: 'Third', viewValue: 'Third'},
+    {value: 'Fourth', viewValue: 'Fourth'}
+  ];
+
+  weekdays = [
+    {value: 'Monday', viewValue: 'Monday'},
+    {value: 'Tuesday', viewValue: 'Tuesday'},
+    {value: 'Wednesday', viewValue: 'Wednesday'},
+    {value: 'Thursday', viewValue: 'Thursday'},
+    {value: 'Friday', viewValue: 'Friday'},
+    {value: 'Saturday', viewValue: 'Saturday'},
+    {value: 'Sunday', viewValue: 'Sunday'}
+  ];
+
+  months = [
+    {value: 'January', viewValue: 'January'},
+    {value: 'February', viewValue: 'February'},
+    {value: 'March', viewValue: 'March'},
+    {value: 'April', viewValue: 'April'},
+    {value: 'May', viewValue: 'May'},
+    {value: 'June', viewValue: 'June'},
+    {value: 'July', viewValue: 'July'},
+    {value: 'August', viewValue: 'August'},
+    {value: 'September', viewValue: 'September'},
+    {value: 'October', viewValue: 'October'},
+    {value: 'November', viewValue: 'November'},
+    {value: 'December', viewValue: 'December'}
+  ];
+
+  // TODO: make this programmatic by counter. may be on ngOnInit or constructor
+  days = [
+    {value: '1', viewValue: '1'},
+    {value: '2', viewValue: '2'},
+    {value: '3', viewValue: '3'},
+    {value: '4', viewValue: '4'},
+    {value: '5', viewValue: '5'},
+    {value: '6', viewValue: '6'},
+    {value: '7', viewValue: '7'},
+    {value: '8', viewValue: '8'},
+    {value: '9', viewValue: '9'},
+    {value: '10', viewValue: '10'},
+    {value: '11', viewValue: '11'},
+    {value: '12', viewValue: '12'},
+    {value: '13', viewValue: '13'},
+    {value: '14', viewValue: '14'},
+    {value: '15', viewValue: '15'},
+    {value: '16', viewValue: '16'},
+    {value: '17', viewValue: '17'},
+    {value: '18', viewValue: '18'},
+    {value: '19', viewValue: '19'},
+    {value: '20', viewValue: '20'},
+    {value: '21', viewValue: '21'},
+    {value: '22', viewValue: '22'},
+    {value: '23', viewValue: '23'},
+    {value: '24', viewValue: '24'},
+    {value: '25', viewValue: '25'},
+    {value: '26', viewValue: '26'},
+    {value: '27', viewValue: '27'},
+    {value: '28', viewValue: '28'},
+    {value: '29', viewValue: '29'},
+    {value: '30', viewValue: '30'},
+    {value: '31', viewValue: '31'}
+  ]
+
   form!: FormGroup;
   id = new FormControl("");
   firstName = new FormControl("", Validators.required);
@@ -173,10 +249,12 @@ export class RemindersComponent implements OnInit {
 
   onSave() {
 
+    // TODO: the record is being saved even through contaqct is blank.
+
     let reminder = this.populateReminder();
 
     if (reminder.id === 0) {
-
+      // TODO: there is bug here with reminder id not being saved.
       this.spin = true;
       this.reminderService.getReminders().subscribe(reminders => {
         reminder.id = reminders[reminders.length - 1].id + 1;
